404406939a4d49b5f4837cdda767ba5718dfbd25
Alexandr <pimenov_1998@list.ru>
2022-10-23 23:42:37 +0300

    Проверочная работа

---
+++ FinalControl/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+
+string[] array = GetArray();
+string[] result = FindLessThan(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] arr, int n) {
+    string[] output = new string[CounString(arr, n)];
+
+    for(int i = 0, j = 0; i < arr.Length; i++) {
+        if(arr[i].Length <= n) {
+            output[j] = arr[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CounString(string[] arr, int n) {
+    int count = 0;
+
+    for(int i = 0; i < arr.Length; i++) {
+        if(arr[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
+string[] GetArray() {
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
