409c17ece139466d62af6d364f480a819870f85f
Alexandr <pimenov_1998@list.ru>
2022-10-24 00:04:31 +0300

    Алгоритм решения

---
+++ FinalControl/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+
+
+string[] GetArray() 
+{
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+
+
+
+int CountLength(string[] arr, int n) 
+{
+    int count = 0;
+    for(int i = 0; i <arr.Length; i++) 
+
+        if(arr[i].Length <= n) count++;
+       
+    return count;
+    
+}
+
+
+string[] FindLessThan(string[] arr, int n) 
+{
+    string[] res = new string[CountLength(arr, n)];
+
+    for(int i = 0, j = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length <= n) {
+            res[j] = arr[i];
+            j++;
+        }
+    }
+    return res;
+}
+
+
+string[] array = GetArray();
+string[] result = FindLessThan(array, 3);
+
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
